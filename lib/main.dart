7de3c8a71ca3380182288bb9756f45a1f2810326
dj-kusuha <dj.kusuha+github@gmail.com>
2024-11-25 00:51:03 +0900

    feature: Random を用いて抽選を試すところまで

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          leading: const Icon(Icons.emoji_emotions_outlined),
+          title: const Text('抽選くん'),
+        ),
+        body: const Center(
+          child: ButtonTest(),
         ),
       ),
     );
+  }
+}
+
+// NOTE: 抽選をしてヒストリーに追加していくのを表示するテスト
+// このあと最大値を入力できるようにしたり、ボックスガチャになるように修正していく
+// ボックスガチャは、抽選対象番号をリストに用意してその中で抽選したあと、
+// 抽選された番号をリストから消していけばよいだろう
+class ButtonTest extends HookWidget {
+  const ButtonTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final random = Random();
+    final count = useState(0);
+    final history = useState(<int>[]);
+
+    return Column(
+      children: [
+        Text('${count.value}'),
+        ElevatedButton(
+          onPressed: () {
+            count.value = random.nextInt(6) + 1;
+            history.value = [...history.value, count.value];
+          },
+          child: const Text('button'),
+        ),
+        Text(_generateHistoryText(history.value)),
+      ],
+    );
+  }
+
+  String _generateHistoryText(List<int> history) {
+    final buf = StringBuffer();
+    buf.write('[');
+    for (final value in history) {
+      buf.write(value);
+      buf.write(', ');
+    }
+    buf.write(']');
+    return buf.toString();
   }
 }
